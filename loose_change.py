9d919f4931d5542ba0ad0ab924d34f119d4b7f58
yjb23tech <yjb23tech@gmail.com>
2023-11-09 00:06:25 +0000

    Learning so much - crazy crazy

--- loose_change.py
+++ loose_change.py
@@ -4,7 +4,12 @@ from weapon import Cutlass, Axe, Dagger
 test_player = Player("Monkey D Luffy", "Romance Dawn Island", 29, 1, 1)
 print(test_player)
 
-test_player.show_weapons_in_armoury()
+test_cutlass = Cutlass()
+print(test_cutlass)
 
+test_axe = Axe()
+print(test_axe)
 
+test_dagger = Dagger()
+print(test_dagger)
 
--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+from weapon import Cutlass, Axe, Dagger
+
 class Player:
 
     def __init__(self, str_name, str_origin, int_age, int_loc_x, int_loc_y):
@@ -10,11 +12,19 @@ class Player:
         self.int_loc_y = int_loc_y 
 
         self.weapon_weapon_in_hand = None
-        self.arr_armoury = []
+        self.arr_armoury = [Cutlass(), Axe(), Dagger()]
 
         self.int_hp = 100
     
     def __str__(self):
 
         return (f"Call me Captain {self.str_name} of {self.str_origin} :D")
+    
+    def show_weapons_in_armoury(self):
+
+        x = 1
+        for weapon in self.arr_armoury:
+            print(weapon)
+
+
 
